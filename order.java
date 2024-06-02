4c0e6af3294a7cbac15cb1952e085eeece7442e1
wlqrksek <130579340+wlqrksek@users.noreply.github.com>
2024-06-03 08:56:11 +0900

    Update order.java

--- order.java
+++ order.java
@@ -3,61 +3,62 @@ import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.ArrayList;
+import java.util.Date;
 
-
-public class order extends JPanel { //장바구니 패널
+public class order extends JPanel {
     static final int MAX_SIZE = 100;
     ArrayList<Menu> orderList;
     Date today;
-    static int orderNum =0;
+    static int orderNum = 0;
     SimpleDateFormat form;
-    static JPanel alermPanel;
-    static JPanel basketPanel;
+    static JPanel alermPanel = new JPanel();
+    static JPanel basketPanel = new JPanel();
+
     public order() {
         init();
         today = new Date();
-        orderList = new ArrayList<Menu>();
+        orderList = new ArrayList<>();
         form = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss a");
-        //order 정보. form.format(today);
         add(alermPanel);
         add(basketPanel);
     }
-    static void init(){
-         JLabel noItems = new JLabel("no items in basket");
-         JLabel yes = new JLabel("확인");
-         alermPanel.setLayout(new FlowLayout());
-         alermPanel.setSize(200,50);
-         alermPanel.add(noItems,yes);
-         alermPanel.setVisible(false);
-         yes.addMouseListener(new MouseAdapter() {
-             @Override
-             public void mouseClicked(MouseEvent e) {
-                 alermPanel.setVisible(false);
-             }
-         });
-         basketPanel.setLayout(new GridLayout(10,1));
-         basketPanel.setSize(500,100);
 
+    static void init() {
+        JLabel noItems = new JLabel("no items in basket");
+        JLabel yes = new JLabel("확인");
+        alermPanel.setLayout(new FlowLayout());
+        alermPanel.setSize(200, 50);
+        alermPanel.add(noItems);
+        alermPanel.add(yes);
+        alermPanel.setVisible(false);
+        yes.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                alermPanel.setVisible(false);
+            }
+        });
+        basketPanel.setLayout(new GridLayout(10, 1));
+        basketPanel.setSize(500, 100);
     }
-    void goToPay(){
-        if(orderList.size()!=0){
-        //결제시스템으로 넘김
-        }
-        else{
+
+    void goToPay() {
+        if (orderList.size() != 0) {
+            // 결제 시스템으로 넘김
+        } else {
             alermPanel.setVisible(true);
-
         }
     }
-    Date getDate(){ //영수증 정보 확인
+
+    Date getDate() {
         return today;
     }
-    void allCancel(){ //장바구니 내의 항목을 지움.
 
-    }
-    void showItems(){ // 장바구니 항목을 보여줌.
-
+    void allCancel() {
+        // 장바구니 내의 항목을 지움.
     }
 
+    void showItems() {
+        // 장바구니 항목을 보여줌.
+    }
 }
