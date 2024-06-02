12d62c8671da519902647c3df33752b163fdb0f0
wlqrksek <130579340+wlqrksek@users.noreply.github.com>
2024-06-03 08:55:18 +0900

    Update Option.java

--- Option.java
+++ Option.java
@@ -1,5 +1,3 @@
-
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -10,22 +8,23 @@ public class Option extends JPanel {
     private JPanel cardPanel;
     private final static int PANEL_WIDTH = 400;
     private final static int PANEL_HEIGHT = 500;
-    String[] size={"small","regular","large"};
+    String[] size = {"small", "regular", "large"};
     String[] sidedish = {"potatoChip", "chicken nugget", "cheese stick"};
-    String[] beverage = {"coke","sida"};
+    String[] beverage = {"coke", "sida"};
     JComboBox<String> option1;
     JComboBox<String> option2;
-    String op1Data,op2Data;
+    String op1Data, op2Data;
+
     public Option() {
         cardLayout = new CardLayout();
         cardPanel = new JPanel(cardLayout);
-        for(int i = 0; i <3; i++){
+        for (int i = 0; i < 3; i++) {
             init(i);
         }
     }
 
     void init(int i) {
-        setPreferredSize(new Dimension(PANEL_WIDTH,PANEL_HEIGHT));
+        setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
         JPanel buttonPanel = new JPanel();
         JButton btnCancel = new JButton("취소");
         JButton btnSelect = new JButton("확인");
@@ -48,27 +47,24 @@ public class Option extends JPanel {
         buttonPanel.add(btnSelect);
         add(buttonPanel, BorderLayout.SOUTH);
 
-        if(i==1) {
+        if (i == 1) {
             option1 = new JComboBox<>(size);
             option2 = new JComboBox<>(beverage);
-        }
-        else if(i==2) {
+        } else if (i == 2) {
             option1 = new JComboBox<>(size);
             option2 = new JComboBox<>(sidedish);
-        }
-        else{
+        } else {
             option1 = new JComboBox<>(beverage);
             option2 = new JComboBox<>(size);
         }
 
         setLayout(new BorderLayout());
-        add(option1,BorderLayout.CENTER);
-        add(option2,BorderLayout.CENTER);
+        add(option1, BorderLayout.CENTER);
+        add(option2, BorderLayout.CENTER);
         setVisible(false);
     }
+
     public void showPage(String pageName) {
         cardLayout.show(cardPanel, pageName);
     }
-
-
 }
