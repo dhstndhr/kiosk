828f92ff01c5ab2672fc6e4fbd958ed53e031b7c
wlqrksek <130579340+wlqrksek@users.noreply.github.com>
2024-06-03 13:15:50 +0900

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -7,13 +7,18 @@ public class Menu extends JPanel {
     private String menuName;
     private int menuPrice;
     private String imagePath;
-    private JPanel basketArea;
+    private order orderPanel;
+    private JPanel mainPanel;
+    private CardLayout cardLayout;
+    private Option optionPanel;
 
-    public Menu(String menuName, String imagePath, int menuPrice, JPanel basketArea) {
+    public Menu(String menuName, String imagePath, int menuPrice, order orderPanel, JPanel mainPanel, CardLayout cardLayout) {
         this.menuPrice = menuPrice;
         this.menuName = menuName;
         this.imagePath = imagePath;
-        this.basketArea = basketArea;
+        this.orderPanel = orderPanel;
+        this.mainPanel = mainPanel;
+        this.cardLayout = cardLayout;
 
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
@@ -26,48 +31,49 @@ public class Menu extends JPanel {
         addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                showOptions();
+                if (menuName.equals("불고기 버거") || menuName.equals("새우 버거")) {
+                    showSetSingleSelection();
+                } else {
+                    orderPanel.addToOrder(menuName + " 단품", null, null, menuPrice);
+                }
             }
         });
     }
 
-    private void showOptions() {
-        JFrame optionFrame = new JFrame("옵션 선택");
-        optionFrame.setSize(300, 200);
-        optionFrame.setLayout(new GridLayout(2, 1));
-        optionFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        optionFrame.setResizable(false);
-
+    private void showSetSingleSelection() {
+        JPanel selectionPanel = new JPanel(new GridLayout(2, 1));
         JButton setButton = new JButton("세트");
         JButton singleButton = new JButton("단품");
 
-        setButton.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                System.out.println(menuName + " 세트 선택됨");
-                optionFrame.dispose();
-            }
+        setButton.addActionListener(e -> {
+            showSetOptions();
+            cardLayout.show(mainPanel, "optionPanel");
         });
 
-        singleButton.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                System.out.println(menuName + " 단품 선택됨");
-                addToBasket(menuName);
-                optionFrame.dispose();
-            }
+        singleButton.addActionListener(e -> {
+            orderPanel.addToOrder(menuName + " 단품", null, null, menuPrice);
+            cardLayout.show(mainPanel, "menuPanel");
         });
 
-        optionFrame.add(setButton);
-        optionFrame.add(singleButton);
-        optionFrame.setLocationRelativeTo(null);
-        optionFrame.setVisible(true);
+        selectionPanel.add(setButton);
+        selectionPanel.add(singleButton);
+
+        mainPanel.add(selectionPanel, "selectionPanel");
+        cardLayout.show(mainPanel, "selectionPanel");
     }
 
-    private void addToBasket(String itemName) {
-        basketArea.add(new JLabel(itemName));
-        basketArea.revalidate();
-        basketArea.repaint();
+    private void showSetOptions() {
+        optionPanel = new Option(() -> {
+            String selectedSide = optionPanel.getSelectedSide();
+            String selectedDrink = optionPanel.getSelectedDrink();
+            if (selectedSide != null && selectedDrink != null) {
+                orderPanel.addToOrder(menuName + " 세트", selectedSide, selectedDrink, menuPrice + 2000); // 세트 가격 추가
+                cardLayout.show(mainPanel, "menuPanel");
+            }
+        });
+
+        mainPanel.add(optionPanel, "optionPanel");
+        cardLayout.show(mainPanel, "optionPanel");
     }
 
     public String getMenuName() {
