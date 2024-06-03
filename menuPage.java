4c49ce3e55aa2f2278ef4ed0ed1bbc9ee3c7b809
wlqrksek <130579340+wlqrksek@users.noreply.github.com>
2024-06-03 13:16:05 +0900

    Update menuPage.java

--- menuPage.java
+++ menuPage.java
@@ -4,13 +4,14 @@ import java.awt.*;
 public class menuPage extends JPanel {
     private CardLayout cardLayout;
     private JPanel cardPanel;
-    private JPanel basketArea;
+    private order orderPanel;
+    private JPanel mainPanel;
 
-
-    public menuPage(String[] pageNames, int[] menuNums, JPanel basketArea) {
-        this.basketArea = basketArea;
-        cardLayout = new CardLayout();
-        cardPanel = new JPanel(cardLayout);
+    public menuPage(String[] pageNames, int[] menuNums, order orderPanel, JPanel mainPanel, CardLayout cardLayout) {
+        this.orderPanel = orderPanel;
+        this.mainPanel = mainPanel;
+        this.cardLayout = cardLayout;
+        this.cardPanel = new JPanel(cardLayout);
 
         for (String pageName : pageNames) {
             JPanel page = createMenuPage(pageName);
@@ -24,14 +25,14 @@ public class menuPage extends JPanel {
     private JPanel createMenuPage(String pageName) {
         JPanel page = new JPanel(new GridLayout(1, 2)); // 두 개의 메뉴만 표시하도록 GridLayout 설정
         if (pageName.equals("HAMBURGER")) {
-            page.add(new Menu("불고기 버거", "img/불고기 버거.png", 5000, basketArea));
-            page.add(new Menu("새우 버거", "img/새우버거.png", 6000, basketArea));
+            page.add(new Menu("불고기 버거", "img/불고기 버거.png", 5000, orderPanel, mainPanel, cardLayout));
+            page.add(new Menu("새우 버거", "img/새우버거.png", 6000, orderPanel, mainPanel, cardLayout));
         } else if (pageName.equals("SIDEDISH")) {
-            page.add(new Menu("감자튀김", "img/감자튀김.png", 2000, basketArea));
-            page.add(new Menu("치즈스틱", "img/치즈스틱.png", 3000, basketArea));
+            page.add(new Menu("감자튀김", "img/감자튀김.png", 2000, orderPanel, mainPanel, cardLayout));
+            page.add(new Menu("치즈스틱", "img/치즈스틱.png", 3000, orderPanel, mainPanel, cardLayout));
         } else if (pageName.equals("BEVERAGE")) {
-            page.add(new Menu("콜라", "img/콜라.png", 1500, basketArea));
-            page.add(new Menu("사이다", "img/사이다.png", 2000, basketArea));
+            page.add(new Menu("콜라", "img/콜라.png", 1500, orderPanel, mainPanel, cardLayout));
+            page.add(new Menu("사이다", "img/사이다.png", 2000, orderPanel, mainPanel, cardLayout));
         }
         return page;
     }
